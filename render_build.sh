f265a7857bc3a3bbce0527ceefe46793228c6165
Tsvetan Kirilov Georgiev <152284852+SertoriusX@users.noreply.github.com>
2026-02-06 14:17:07 +0100

    Update render_build.sh

--- render_build.sh
+++ render_build.sh
@@ -3,8 +3,16 @@
 set -o errexit
 
 npm install
+npm install axios
+
 npm run build
 
-pipenv install
+pipenv install --dev --deploy --ignore-pipfile
+psql "$DATABASE_URL" -c "DROP SCHEMA public CASCADE;"
+psql "$DATABASE_URL" -c "CREATE SCHEMA public;"
 
+pipenv run flask db stamp head
+pipenv install google-generativeai
+pipenv install openai
+pipenv run migrate
 pipenv run upgrade
